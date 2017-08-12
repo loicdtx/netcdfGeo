e4472a5fc9031eaa2954c8c071f769e122d54dff
Loïc Dutrieux <loic.dutrieux@gmail.com>
2017-08-11 19:34:47 -0500

    Renamed package to valid name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='netcdf-geo',
+setup(name='netcdfGeo',
       version='0.0.1a',
       description='Gdal compliant handling of projected spatio-temporal data in NetCDF',
       long_description='TODO',
@@ -12,7 +12,7 @@ setup(name='netcdf-geo',
       license='GPLv3',
       packages=find_packages(exclude=['tests']),
       install_requires=[
-          'rasterio==1.0a8',
+          'rasterio==1.0a9',
           'numpy',
           'netCDF4',
           'pyproj',
